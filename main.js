5ac8335542b3ff1d885d6fc319f13ea201b5314a
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-07-03 17:33:13 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+
+/*1.Crie a função setup 
+2. Crie um canva com 280 de largura e 280 de altura
+3.Coloque o canva no center
+4.Defina a cor de fundo para branco
+5.Utilize o código ' canvas.mouseReleased(classifyCanvas)' para que no seu canva
+seja aplicado o 'classifyCanvas'
+6.Utilize o código ' synth = window.speechSynthesis' para colocar a api da voz no seu site.*/
+
+/*1.Crie a função preload 
+2.Utilize o código 'classifier = ml5.imageClassifier('DoodleNet');' para aplicar 
+o modelo pré treinado 'DoodleNet'*/
+
+/*1. Crie a função clearCanvas 
+2. Defina a cor de fundo de branco para branco*/
+
